8cce534ba0f66bdc4d88bc3baf736a7ec7b6fd5b
Sala K 301 <salak301@salasfr.itm.edu.co>
2024-08-17 14:48:35 -0500

    Add Entidad model

---
+++ Models/Entidad.cs
@@ -0,0 +1,44 @@
+#nullable enable
+
+using System.Collections.Generic;
+
+namespace ProyectoBackend.Models
+{
+
+    public class Entidad
+    {
+        private Dictionary<string, object?> propiedades;
+
+        public Entidad()
+        {
+            propiedades = new Dictionary<string, object?>();
+        }
+
+        public Entidad(Dictionary<string, object?> initialPropiedades)
+        {
+            propiedades = initialPropiedades ?? new Dictionary<string, object?>();
+        }
+
+        public object? this[string nombre]
+        {
+            get
+            {
+                if (propiedades.TryGetValue(nombre, out var value))
+                {
+                    return value;
+                }
+                return null;
+            }
+            set
+            {
+                propiedades[nombre] = value;
+            }
+        }
+        public Dictionary<string, object?> ObtenerPropiedades()
+        {
+            return new Dictionary<string, object?>(propiedades);
+        }
+
+    }
+
+}
